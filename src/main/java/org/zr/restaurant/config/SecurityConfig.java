c7fa3419a1528625a6327e03ad059982359a19b8
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-09 00:26:12 +0530

    made access of GET method of PhotoController to public

--- src/main/java/org/zr/restaurant/config/SecurityConfig.java
+++ src/main/java/org/zr/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.zr.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +18,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(
-                requests -> requests.anyRequest().authenticated()
+                requests ->
+                        requests.requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(
                         oauth2 -> oauth2.jwt(
