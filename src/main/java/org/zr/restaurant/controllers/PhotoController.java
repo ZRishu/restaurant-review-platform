0eeb29db0d53f036bbe7c5c4e6ae28049d3e56f8
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-09 00:25:20 +0530

    add GET method to retrieve photo in PhotoController

--- src/main/java/org/zr/restaurant/controllers/PhotoController.java
+++ src/main/java/org/zr/restaurant/controllers/PhotoController.java
@@ -1,10 +1,12 @@
 package org.zr.restaurant.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.zr.restaurant.domain.dtos.PhotoDto;
 import org.zr.restaurant.domain.entities.Photo;
@@ -23,5 +25,18 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file")MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(
+                                MediaTypeFactory.getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 }
