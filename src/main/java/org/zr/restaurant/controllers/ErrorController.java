c167c860295efd53ef4d30acf3e0c7e62e4c64d3
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-08 23:54:52 +0530

    add Exception Handler to handle Storage Exception

---
+++ src/main/java/org/zr/restaurant/controllers/ErrorController.java
@@ -0,0 +1,27 @@
+package org.zr.restaurant.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.zr.restaurant.domain.dtos.ErrorDto;
+import org.zr.restaurant.exceptions.StorageException;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(StorageException.class)
+    public ResponseEntity<ErrorDto> handleStorageException(StorageException ex) {
+        log.error("Caught StorageException", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("Unable to save or retrieve resources at this time")
+                .build();
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
