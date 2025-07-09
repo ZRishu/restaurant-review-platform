40f6741868b2308f6108863bdef595ad7843ea77
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-09 22:22:51 +0530

    add DELETE method in RestController to delete Restaurant

--- src/main/java/org/zr/restaurant/controllers/RestaurantController.java
+++ src/main/java/org/zr/restaurant/controllers/RestaurantController.java
@@ -74,4 +74,12 @@ public class RestaurantController {
 
         return ResponseEntity.ok(restaurantMapper.toRestaurantDto(updateRestaurant));
     }
+
+    @DeleteMapping(path = "/{restaurant_id}")
+    public ResponseEntity<Void> deleteRestaurant(
+            @PathVariable("restaurant_id") String restaurantId
+    ) {
+        restaurantService.deleteRestaurant(restaurantId);
+        return ResponseEntity.noContent().build();
+    }
 }
