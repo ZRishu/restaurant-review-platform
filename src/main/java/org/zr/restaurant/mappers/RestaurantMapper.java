7bb151cdb6196fefec8548c71996c1a8242f52be
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-09 20:19:09 +0530

    create method to map Restaurant to RestaurantSummaryDto

--- src/main/java/org/zr/restaurant/mappers/RestaurantMapper.java
+++ src/main/java/org/zr/restaurant/mappers/RestaurantMapper.java
@@ -2,19 +2,32 @@ package org.zr.restaurant.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.zr.restaurant.domain.RestaurantCreateUpdateRequest;
 import org.zr.restaurant.domain.dtos.GeoPointDto;
 import org.zr.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
 import org.zr.restaurant.domain.dtos.RestaurantDto;
+import org.zr.restaurant.domain.dtos.RestaurantSummaryDto;
 import org.zr.restaurant.domain.entities.Restaurant;
+import org.zr.restaurant.domain.entities.Review;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
     RestaurantCreateUpdateRequest toRestaurantCreateUpdateRequest(RestaurantCreateUpdateRequestDto dto);
 
     RestaurantDto toRestaurantDto(Restaurant restaurant);
+
+    @Mapping(source = "reviews", target = "totalReviews", qualifiedByName = "populateTotalReviews")
+    RestaurantSummaryDto toSummaryDto(Restaurant restaurant);
+
+    @Named("populateTotalReviews")
+    default Integer populateTotalReviews(List<Review> reviews) {
+        return reviews.size();
+    }
 
     @Mapping(target = "latitude", expression = "java(geoPoint.getLat())")
     @Mapping(target = "longitude", expression = "java(geoPoint.getLon())")
