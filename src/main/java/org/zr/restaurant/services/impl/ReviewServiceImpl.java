61d7abf37a04219fcedc6fb31f4ade8660abfb1f
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-10 00:17:05 +0530

    create ReviewServiceImpl class and implement createReview method

---
+++ src/main/java/org/zr/restaurant/services/impl/ReviewServiceImpl.java
@@ -0,0 +1,89 @@
+package org.zr.restaurant.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zr.restaurant.domain.ReviewCreateUpdateRequest;
+import org.zr.restaurant.domain.entities.Photo;
+import org.zr.restaurant.domain.entities.Restaurant;
+import org.zr.restaurant.domain.entities.Review;
+import org.zr.restaurant.domain.entities.User;
+import org.zr.restaurant.exceptions.RestaurantNotFoundException;
+import org.zr.restaurant.exceptions.ReviewNotAllowedException;
+import org.zr.restaurant.repositories.RestaurantRepository;
+import org.zr.restaurant.services.ReviewService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+@RequiredArgsConstructor
+public class ReviewServiceImpl implements ReviewService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    @Transactional
+    @Override
+    public Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review) {
+        Restaurant restaurant = getRestaurantOrThrow(restaurantId);
+
+        boolean hasExistingReview = restaurant.getReviews()
+                .stream()
+                .anyMatch(r -> r.getWrittenBy().getId().equals(author.getId()));
+
+        if (hasExistingReview) {
+            throw new ReviewNotAllowedException("User has already reviewed this restaurant");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Photo> photos = review.getPhotoIds().stream().map(url -> Photo.builder()
+                .url(url)
+                .uploadDate(now)
+                .build()
+        ).toList();
+
+        String reviewId = UUID.randomUUID().toString();
+        Review reviewToCreate = Review.builder()
+                .id(reviewId)
+                .content(review.getContent())
+                .rating(review.getRating())
+                .photos(photos)
+                .writtenBy(author)
+                .datePosted(now)
+                .lastEdited(now)
+                .build();
+
+        restaurant.getReviews().add(reviewToCreate);
+
+        updateRestaurantAverageRating(restaurant);
+
+        Restaurant savedRestaurant = restaurantRepository.save(restaurant);
+
+        return savedRestaurant.getReviews().stream()
+                .filter(r -> reviewId.equals(r.getId()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Error retrieving created review"));
+    }
+
+    private Restaurant getRestaurantOrThrow(String restaurantId) {
+        return restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new RestaurantNotFoundException(
+                        "Restaurant with id: " + restaurantId + " not found")
+                );
+    }
+
+    private void updateRestaurantAverageRating(Restaurant restaurant) {
+        List<Review> reviews = restaurant.getReviews();
+        if (reviews.isEmpty()) {
+            restaurant.setAverageRating(0.0f);
+        } else {
+            double averageRating = reviews.stream().mapToDouble(Review::getRating)
+                    .average()
+                    .orElse(0.0);
+            restaurant.setAverageRating((float) averageRating);
+        }
+    }
+}
