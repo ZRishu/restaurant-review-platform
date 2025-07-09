5c141589391c92047ec5f380bf1795c16158aec0
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-09 20:16:44 +0530

    create method in RestaurantService class to get restaurant search feature

--- src/main/java/org/zr/restaurant/services/RestaurantService.java
+++ src/main/java/org/zr/restaurant/services/RestaurantService.java
@@ -1,8 +1,20 @@
 package org.zr.restaurant.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.zr.restaurant.domain.RestaurantCreateUpdateRequest;
 import org.zr.restaurant.domain.entities.Restaurant;
 
 public interface RestaurantService {
+
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
+
+    Page<Restaurant> searchRestaurant(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+    );
 }
