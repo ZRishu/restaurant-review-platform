214a56bc261c505a98f8d164527a8a88ff0a673f
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-10 13:14:46 +0530

    add list reviews method in ReviewService class

--- src/main/java/org/zr/restaurant/services/ReviewService.java
+++ src/main/java/org/zr/restaurant/services/ReviewService.java
@@ -1,9 +1,13 @@
 package org.zr.restaurant.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.zr.restaurant.domain.ReviewCreateUpdateRequest;
 import org.zr.restaurant.domain.entities.Review;
 import org.zr.restaurant.domain.entities.User;
 
 public interface ReviewService {
     Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review);
+
+    Page<Review> listReviews(String restaurantId, Pageable pageable);
 }
